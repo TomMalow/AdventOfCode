8a344b3f7ec9c79b4eba414ed9a38a45b28e6c9b
Thomas Malowanczyk <tkm@delegate.dk>
2025-12-06 19:09:39 +0100

    feat(day1): add solution to part 2

--- 2025/day1/main.py
+++ 2025/day1/main.py
@@ -21,8 +21,37 @@ def solve_part1(pointer_start, operations):
 
     return zeroCounter
 
+def solve_part2(pointer_start, operations):
+    pointer = pointer_start
+    zeroCounter = 0
+
+    for operation in operations:
+        direction = operation[:1]
+        distance = int(operation[1:])
+        sign = -1 if direction == 'L' else 1
+        change = (distance * sign)
+
+        quotient = (pointer + change) // 100
+        new_pointer = (pointer + change) % 100
+
+        # handle starting at 0 case for left rotation
+        if pointer == 0 and quotient < 0:
+            quotient += 1
+
+        # handle ending at 0 case when going left
+        if new_pointer == 0 and quotient <= 0:
+            zeroCounter += 1
+
+        zeroCounter += abs(quotient)
+        pointer = new_pointer
+
+
+    return zeroCounter
+
 if __name__ == '__main__':
     file_path = sys.argv[1]
     operations = parse_file(file_path)
     result_part1 = solve_part1(50, operations)
     print(f"Part 1: {result_part1}, should be 3")
+    result_part2 = solve_part2(50, operations)
+    print(f"Part 2: {result_part2}, should be 6")
--- 2025/day1/test.py
+++ 2025/day1/test.py
@@ -36,5 +36,75 @@ class TestPart1(unittest.TestCase):
         self.assertEqual(result, 0)
 
 
+class TestPart2(unittest.TestCase):
+
+    def test_single_quater_left_rotation(self):
+        result = main.solve_part2(50, ['L25'])
+        self.assertEqual(result, 0)
+
+    def test_single_half_left_rotation(self):
+        result = main.solve_part2(50, ['L50'])
+        self.assertEqual(result, 1)
+
+    def test_single_full_left_rotation(self):
+        result = main.solve_part2(50, ['L100'])
+        self.assertEqual(result, 1)
+
+    def test_two_full_left_rotation(self):
+        result = main.solve_part2(50, ['L200'])
+        self.assertEqual(result, 2)
+    
+    def test_single_quater_Right_rotation(self):
+        result = main.solve_part2(50, ['R25'])
+        self.assertEqual(result, 0)
+
+    def test_single_half_right_rotation(self):
+        result = main.solve_part2(50, ['R50'])
+        self.assertEqual(result, 1)
+
+    def test_single_full_right_rotation(self):
+        result = main.solve_part2(50, ['R100'])
+        self.assertEqual(result, 1)
+
+    def test_two_full_right_rotation(self):
+        result = main.solve_part2(50, ['R200'])
+        self.assertEqual(result, 2)
+
+    def test_0_to_99_left_rotation(self):
+        result = main.solve_part2(0, ['L99'])
+        self.assertEqual(result, 0)
+
+    def test_0_to_100_left_rotation(self):
+        result = main.solve_part2(0, ['L100'])
+        self.assertEqual(result, 1)
+
+    def test_0_to_5_left_rotation(self):
+        result = main.solve_part2(0, ['L5'])
+        self.assertEqual(result, 0)
+
+    def test_1_to_5_left_rotation(self):
+        result = main.solve_part2(1, ['L5'])
+        self.assertEqual(result, 1)
+
+    def test_50_to_1000_left_rotation(self):
+        result = main.solve_part2(50, ['L1000'])
+        self.assertEqual(result, 10)
+    
+    def test_50_to_1000_right_rotation(self):
+        result = main.solve_part2(50, ['R1000'])
+        self.assertEqual(result, 10)
+
+    def test_0_to_100_right_rotation(self):
+        result = main.solve_part2(0, ['R200'])
+        self.assertEqual(result, 2)
+    
+    def test_0_to_105_left_rotation(self):
+        result = main.solve_part2(0, ['L105'])
+        self.assertEqual(result, 1)
+
+    def test_5_to_5_left_rotation(self):
+        result = main.solve_part2(5, ['L105'])
+        self.assertEqual(result, 2)
+
 if __name__ == '__main__':
     unittest.main()
