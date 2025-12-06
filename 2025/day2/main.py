e5a4906761ea2366f6ad46b5e5b674d100a32ed3
Thomas Malowanczyk <tkm@delegate.dk>
2025-12-06 21:39:51 +0100

    feat(day2): add solution to part 2

--- 2025/day2/main.py
+++ 2025/day2/main.py
@@ -35,6 +35,58 @@ def solve_part1(ranges):
 
     return sum
 
+def solve_part2(ranges):
+
+    divisors_cache = {}
+    sum = 0
+    for r in ranges:
+        str_numbers = generate_range_numbers_as_strings(r)
+        matches = []
+        for n in str_numbers:
+            c_len = len(n)
+
+            # get all divisors of c_len excluding c_len itself
+            divisors = divisors_cache.get(c_len)
+            if divisors is None:
+                divisors = generate_divisors(c_len) 
+                divisors_cache[c_len] = divisors    
+
+            # generate all sub strings based on divisors
+            generated_substrings = generate_substrings(n, divisors)
+
+            for parts in generated_substrings:
+
+                if check_sub_parts(parts):
+                    matches.append(n)
+                    sum += int(n)
+                    break 
+
+    return sum
+
+def check_sub_parts(parts):
+    first = parts[0]
+    for part in parts:
+        if first != part:
+            return False
+            break
+    return True
+
+def generate_substrings(n, divisors):
+    potential_parts = []
+    for d in divisors:
+        parts = []
+        for i in range(0, len(n), d):
+            parts.append(n[i:i+d])
+        potential_parts.append(parts)
+    return potential_parts
+
+def generate_divisors(n):
+    divisors = []
+    for i in range(1, n):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
+    
 def generate_range_numbers_as_strings(r):
     return list(map(str, range(r[0], r[1]+1)))
 
@@ -43,3 +95,5 @@ if __name__ == '__main__':
     ranges = parse_file(file_path)
     result_part1 = solve_part1(ranges)
     print(f"Part 1: {result_part1}, should be 1227775554")
+    result_part2 = solve_part2(ranges)
+    print(f"Part 2: {result_part2}, should be 4174379265")
--- 2025/day2/test.py
+++ 2025/day2/test.py
@@ -35,5 +35,51 @@ class TestPart1(unittest.TestCase):
         result = main.solve_part1([[38593856, 38593862]])
         self.assertEqual(result, 38593859)
 
+class TestPart2(unittest.TestCase):
+
+    def test_11_22(self):
+        result = main.solve_part2([[11, 22]])
+        self.assertEqual(result, 11 + 22)
+
+    def test_95_115(self):
+        result = main.solve_part2([[95, 115]])
+        self.assertEqual(result, 99 + 111)
+
+    def test_998_1012(self):
+        result = main.solve_part2([[998, 1012]])
+        self.assertEqual(result, 999 + 1010)
+
+    def test_1188511880_1188511890(self):
+        result = main.solve_part2([[1188511880, 1188511890]])
+        self.assertEqual(result, 1188511885)
+
+    def test_222220_222224(self):
+        result = main.solve_part2([[222220, 222224]])
+        self.assertEqual(result, 222222)
+
+    def test_1698522_1698528(self):
+        result = main.solve_part2([[1698522, 1698528]])
+        self.assertEqual(result, 0)
+
+    def test_446443_446449(self):
+        result = main.solve_part2([[446443, 446449]])
+        self.assertEqual(result, 446446)
+
+    def test_38593856_38593862(self):
+        result = main.solve_part2([[38593856, 38593862]])
+        self.assertEqual(result, 38593859)
+
+    def test_565653_565659(self):
+        result = main.solve_part2([[565653, 565659]])
+        self.assertEqual(result, 565656)
+
+    def test_824824821_824824827(self):
+        result = main.solve_part2([[824824821, 824824827]])
+        self.assertEqual(result, 824824824)
+
+    def test_2121212118_2121212124(self):
+        result = main.solve_part2([[2121212118, 2121212124]])
+        self.assertEqual(result, 2121212121)
+
 if __name__ == '__main__':
     unittest.main()
